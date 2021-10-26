de837e148d228d8303b1020074ce4fbde8c5bd7f
YampolskyiDmitriy <yadima2001@gmail.com>
2021-10-26 20:15:01 +0300

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Dima";
+            string surname = "Yampolskyi";
+            int age = 19;
+            int result = 40 - age;
+            Console.WriteLine($"Name: " + name + "\nSurname: " + surname + "\nAge: " + age + "\nYears till 40: " + result);
+        }
+    }
+}
