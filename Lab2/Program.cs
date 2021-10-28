37da1b00ffe084f69b6cf51b1299404086c75986
YampolskyiDmitriy <yadima2001@gmail.com>
2021-10-28 17:48:23 +0300

    Lab2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,7 +6,48 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            var random = new Random();
+            int[] arrN = new int[20];
+            int count = 0;
+            Console.WriteLine("arrN");
+            for(int i = 0; i < arrN.Length; i++)
+            {
+                arrN[i] = random.Next(-200, 200);
 
+                Console.WriteLine($"{i}:{arrN[i]}, ");
+
+                if( arrN[i] > -101 && arrN[i] < 100)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Numbers in range from -100 to 100: {count}");
+
+
+            int[] arrA = new int[20];
+            int[] arrB = new int[20];
+            int bCounter = 0;
+            Console.WriteLine("arrA");
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                arrA[i] = random.Next(2000);
+
+                Console.WriteLine($"{i}:{arrA[i]}, ");
+
+                if (arrA[i] <= 888)
+                {
+                    arrB[bCounter] = arrA[i];
+                    bCounter++;
+                }
+            }
+
+            Console.WriteLine("arrB");
+            for (int i = 0; i < arrB.Length; i++)
+            {
+                Console.WriteLine($"{i}:{arrB[i]}, ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
